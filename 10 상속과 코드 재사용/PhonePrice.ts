dbc5779184871a3ce18de0a24713ddae625d665d
Jo Doul <ryan1357ryan@naver.com>
2024-01-24 21:24:13 +0900

    중복 코드 수정

--- 10 상속과 코드 재사용/PhonePrice.ts
+++ 10 상속과 코드 재사용/PhonePrice.ts
@@ -21,10 +21,12 @@ namespace ch10 {
     private amount: number;
     private seconds: number;
     private calls: Call[];
+    private taxRate: number;
 
-    constructor(amount: number, seconds: number) {
+    constructor(amount: number, seconds: number, taxRate: number) {
       this.amount = amount;
       this.seconds = seconds;
+      this.taxRate = taxRate;
     }
 
     public call(call: Call) {
@@ -50,7 +52,7 @@ namespace ch10 {
         result += (call.getDuration() / this.seconds) * this.amount;
       }
 
-      return result;
+      return result + (result * this.taxRate);
     }
   }
 
@@ -61,11 +63,13 @@ namespace ch10 {
     private regularAmount: number;
     private seconds: number;
     private calls: Call[];
+    private taxRate: number;
 
-    constructor(nightlyAmount: number, regularAmount: number, seconds: number) {
+    constructor(nightlyAmount: number, regularAmount: number, seconds: number, taxRate: number) {
       this.nightlyAmount = nightlyAmount;
       this.regularAmount = regularAmount;
       this.seconds = seconds;
+      this.taxRate = taxRate;
     }
     // phone과 중복 코드를 가지고 있어 수정사항이 발생하면 둘 다 수정 필요해짐
     public caculateFee(): number {
@@ -78,8 +82,8 @@ namespace ch10 {
           result += (call.getDuration() / this.seconds) * this.regularAmount;
         }
       }
-
-      return result;
+      // 세금 부과 로직을 중복된 코드에 모두 추가함
+      return result + (result * this.taxRate);
     }
   }
 }
