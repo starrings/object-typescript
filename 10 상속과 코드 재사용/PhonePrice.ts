36a3d502a033036ccef0ffa698bee04782f011d8
Jo Doul <ryan1357ryan@naver.com>
2024-01-25 21:47:14 +0900

    상속 추가

--- 10 상속과 코드 재사용/PhonePrice.ts
+++ 10 상속과 코드 재사용/PhonePrice.ts
@@ -20,30 +20,15 @@ namespace ch10 {
   enum PhoneType { REGULAR, NIGHTLY };
 
   class Phone {
-    private static readonly LATE_NIGHT_HOUR = 22;
-    
     private amount: number;
-    private regularAmount: number;
-    private nightlyAmount: number;
     private seconds: number;
     private calls: Call[];
     private taxRate: number;
-    private type: PhoneType;
 
-    constructor(type: PhoneType, seconds: number, amount: number, nightlyAmount: number, regularAmount: number) {
-      if(type === PhoneType.REGULAR) {
-        this.type = type;
-        this.seconds = seconds;
-        this.amount = amount;
-        this.nightlyAmount = 0;
-        this.regularAmount = 0;
-      } else if (type == PhoneType.NIGHTLY) {
-        this.type = type;
-        this.seconds = seconds;
-        this.amount = 0;
-        this.nightlyAmount = nightlyAmount;
-        this.regularAmount = regularAmount;
-      }
+    constructor(seconds: number, amount: number, taxRate: number) {
+      this.seconds = seconds;
+      this.amount = amount;
+      this.taxRate = taxRate;
     }
 
     public call(call: Call) {
@@ -67,50 +52,35 @@ namespace ch10 {
       let result: number = 0;
 
       for (const call of this.calls) {
-        if(this.type = PhoneType.REGULAR) {
-          result += (call.getDuration() / this.seconds) * this.amount;
-        } else {
-          if (call.getFrom().getHours() >= Phone.LATE_NIGHT_HOUR) {
-            result += (call.getDuration() / this.seconds) * this.nightlyAmount;
-          } else {
-            result += (call.getDuration() / this.seconds) * this.regularAmount;
-          }
-        }
-        
+        result += (call.getDuration() / this.seconds) * this.amount;
       }
 
       return result + (result * this.taxRate);
     }
   }
-
-  // class NightDiscountPhone {
-  //   private static readonly LATE_NIGHT_HOUR = 22;
+  // 상속을 이용한 중복 제거
+  class NightDiscountPhone extends Phone {
+    private static readonly LATE_NIGHT_HOUR = 22;
     
-  //   private nightlyAmount: number;
-  //   private regularAmount: number;
-  //   private seconds: number;
-  //   private calls: Call[];
-  //   private taxRate: number;
+    private nightlyAmount: number;
 
-  //   constructor(nightlyAmount: number, regularAmount: number, seconds: number, taxRate: number) {
-  //     this.nightlyAmount = nightlyAmount;
-  //     this.regularAmount = regularAmount;
-  //     this.seconds = seconds;
-  //     this.taxRate = taxRate;
-  //   }
-  //   // phone과 중복 코드를 가지고 있어 수정사항이 발생하면 둘 다 수정 필요해짐
-  //   public caculateFee(): number {
-  //     let result = 0;
+    constructor(nightlyAmount: number, regularAmount: number, seconds: number, taxRate: number) {
+      super(regularAmount, seconds,taxRate);
+      this.nightlyAmount = nightlyAmount;
+    }
+    // phone과 중복 코드를 가지고 있어 수정사항이 발생하면 둘 다 수정 필요해짐
+    public caculateFee(): number {
+      let result: number = super.calculateFee();
+      let nightlyFee = 0;
 
-  //     for (const call of this.calls) {
-  //       if (call.getFrom().getHours() >= NightDiscountPhone.LATE_NIGHT_HOUR) {
-  //         result += (call.getDuration() / this.seconds) * this.nightlyAmount;
-  //       } else {
-  //         result += (call.getDuration() / this.seconds) * this.regularAmount;
-  //       }
-  //     }
-  //     // 세금 부과 로직을 중복된 코드에 모두 추가함
-  //     return result + (result * this.taxRate);
-  //   }
-  // }
+      // 억지로 상속을 하여 코드가 약간 덜 직ㄴ관적임
+      for (const call of this.getCalls()) {
+        if (call.getFrom().getHours() >= NightDiscountPhone.LATE_NIGHT_HOUR) {
+          nightlyFee += (this.getAmount() - this.nightlyAmount) * (call.getDuration() / this.getSeconds());
+        } 
+      }
+      // 세금 부과 로직을 중복된 코드에 모두 추가함
+      return result - nightlyFee;
+    }
+  }
 }
